ff8eee40ada8fc82aacae2774b6959595aaf320f
Renan Aguiar <renan.modesto@grupomultiplica.com.br>
2021-12-11 20:50:43 -0300

    axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header/header";
 import Footer from "./components/Footer/footer";
 import "./assets/index.css";
@@ -7,24 +7,28 @@ import FelizAniversario from "./components/FelizAniversario/Felizaniversario";
 import Cards from "./components/Cards/Cards";
 import api from "./components/services/api";
 
-class App extends Component {
-  state = { filmes: [] };
+function App() {
+  const [filmes, setVideos] = useState([]);
 
-  async componentDidMount() {
-    const response = await api.get("star%20wars");
-    console.log(response.data);
-    this.setState({ filmes: response.data });
-  }
-  render() {
-    return (
-      <section>
-        <Header />
-        <FelizAniversario />
-        <Cards />
-        <Footer />
-      </section>
-    );
-  }
+  useEffect(() => {
+    api.get("star%20wars").then(({ data }) => {
+      setVideos(data);
+    });
+    console.log(filmes);
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  console.log(filmes);
+
+  return (
+    <section>
+      <Header />
+      <FelizAniversario />
+      {filmes.map((filme) => (
+        <Cards key={filme.id} id={filme.id} desc={filme.show.name} foto={filme.show.image.medium}/>
+      ))}
+      <Footer />
+    </section>
+  );
 }
 
 export default App;
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,41 +1,19 @@
 import React, { Component } from "react";
 import Logo from "../../assets/img/disposicao-da-vista-superior-com-um-cartao-emoji-sorridente_23-2148860301.jpg";
 import "./style.css";
-import api from "../services/api";
 
-class Cards extends Component {
-  state = { filmes: [], };
+function Cards ({desc, foto}) {
 
-  async componentDidMount() {
-  const response = await api.get("star%20wars");
-  console.log(response.data);
-this.setState({ filmes: response.data });
- }
-
-  render() {
-    console.log(this.state);
+  
     return (
-      <div>
-         
-        <h1>Listar filmes</h1>
-        {this.state.filmes.forEach((filme) => (
-          <div className="second-section">
+         <div className="second-section">
             <div className="card">
-              <li key={filme.show.id}>
-                <h2>
-                  <strong>Título: </strong>
-                  {filme.show.name}
-                  {filme.show}
-                </h2>
-                <p>{filme.show.url}</p>
-              </li>
-              <img className="img" src={Logo} alt=" " />
+              <p className="description-photo">{desc}</p>
+              <img className="img" src={foto} alt=" " />
             </div>
           </div>
-        ))}
-      </div>
     );
   }
-}
+
 
 export default Cards;
